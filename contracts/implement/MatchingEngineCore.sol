83193e66b7979efe3b293d1d9a4602298398a7a4
MusketPosition <musket.gun@position.exchange>
2022-12-02 21:37:10 +0700

    remove function no used

--- contracts/implement/MatchingEngineCore.sol
+++ contracts/implement/MatchingEngineCore.sol
@@ -732,8 +732,6 @@ abstract contract MatchingEngineCore is MatchingEngineCoreStorage {
         returns (uint256)
     {}
 
-    /// @notice hook function get basis point
-    function getBasisPoint() external view virtual returns (uint256) {}
 
     /// @notice hook function get currnet pip
     function getCurrentPip() external view virtual returns (uint128) {}
