c31b7e090d5aac5733e1c68c3e545a1bec431ba4
MusketPosition <musket.gun@position.exchange>
2023-02-20 14:27:38 +0700

    change name interface

--- contracts/interfaces/IGetFeeShareAMM.sol
+++ contracts/interfaces/IGetFeeShareAMM.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.9;
 
-interface ISpotFactory {
+interface IGetFeeShareAMM {
     /// @notice fee share for liquidity provider
     /// @return the rate share
     function feeShareAmm() external view returns (uint32);
